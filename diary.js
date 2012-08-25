d1bde759b40b31e6ff7fed3d4571a9e65b8fb4ed
gwj <gwj@gwj-System-Product-Name>
2012-08-26 00:34:56 +0800

    add diary

---
+++ diary.js
@@ -0,0 +1,7 @@
+2012-08-25
+	I found another interesting work models:
+		http://www.linuxdeepin.com/
+
+	if modify some config in jb-dev, you can built it under 32bit. :( I found to developt on 64bit machine is a  horrable thing. Becase, you have to let 32bit elf-binary run compatiblly. For this, it mean you have to install twice of files, to compare a 32bit OS.
+
+
