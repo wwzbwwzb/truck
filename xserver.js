713f6e5ad0e5433552dc77b35c19e5467ff62c54
gwj <gwj@web.com>
2012-10-23 13:16:52 +0000

    add test dirs

---
+++ xserver.js
@@ -0,0 +1,38 @@
+
+complict:
+1. xserver is too large as a just a xserver
+2. 
+
+1.old xserver required libraries:
+	dmx
+	Xmu
+	Xi
+	Xext
+	X11
+	Xt
+	Xau
+	Xdmcp
+	Xfixes
+	Xrender
+
+xproto
+randrproto
+renderproto
+xextproto
+inputproto
+kbproto
+fontsproto
+videoproto
+dri2proto
+xineramaproto
+
+inner requires:
+	fb
+	mi
+	render
+	Xi
+	xkb
+	dix
+	miext
+	os
+
